a48adb3172fa41fc785121f54f2b53b6bf5f4183
Elena <elen.zmanovskaya@gmail.com>
2023-04-21 09:18:05 +0700

    Update lists.php

--- pages/lists.php
+++ pages/lists.php
@@ -2,16 +2,12 @@
 foreach (glob("../classes/*.php") as $filename) {
     include_once $filename;
 }
-
-
 $cat = $_POST['cat'];
 $pdo = Tools::connect();
 $items = Item::GetItems($cat);
 if($items == null) exit();
-
 foreach($items as $item)
 {
     $item->Draw();
 }
-
 ?>
